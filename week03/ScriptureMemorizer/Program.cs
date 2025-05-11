3a49f78faaebe0db4eaa9e51d27c96eaf9a8b596
nniumei <nnygtz@byui.edu>
2025-05-10 23:07:33 -0600

    Program.cs

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -2,8 +2,29 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        var reference = new VerseReference("Helaman", 5, 12);
+        string verseLine = "Remember that it is upon the rock of our Redeemer who is Christ the Son of God";
+
+        var session = new MemoryVerse(reference, verseLine);
+
+        while (!session.FullyMasked())
+        {
+            Console.Clear();
+            session.Reveal();
+
+            Console.Write("\nHit [Enter] to continue or type 'done' to exit: ");
+            string input = Console.ReadLine();
+
+            if (input.Trim().ToLower() == "done")
+                break;
+
+            session.ObscureWords(2);
+        }
+
+        Console.Clear();
+        session.Reveal();
+        Console.WriteLine("\nYou’ve completed the memorization round.");
     }
 }
